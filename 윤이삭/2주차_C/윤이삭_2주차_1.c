82b8e07156652f367b220bbeeaa5fa98e25bc58a
Barleyfield <egg12347@naver.com>
2018-04-03 20:04:26 +0900

    2주차 업로드

---
+++ 윤이삭/2주차_C/윤이삭_2주차_1.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#define _CRT_SECURE_NO_WARNING
+
+void amicable(int num1, int num2)		// 우애수 또는 친화수 함수
+{
+	int quotient_sum_1 = 0, quotient_sum_2 = 0;		// 몫 (여기서는 sum으로 몫을 다 더한 후 비교. 배열 사용해도 상관 없음.)
+	int i = 0;
+	for (i = 1; i < num1; i++)
+	{
+		if (num1 % i == 0)
+			quotient_sum_1 += i;	
+	}
+	for (i = 1; i < num2; i++)
+	{
+		if (num2 % i == 0)
+			quotient_sum_2 += i;
+	}
+	
+	if ( (quotient_sum_1 == num2) && (quotient_sum_2 == num1) )
+		printf("두 수는 우애수입니다.\n");
+	else
+		printf("두 수는 우애수가 아닙니다.\n");
+
+
+}
+
+int main()
+{
+	int n1 = 0, n2 = 0;
+	int result = 0;
+	printf("수 2개를 입력하세요 : ");
+	scanf("%d %d", &n1, &n2);
+	amicable(n1, n2);		// 우애수 함수를 사용합니다. (반환형은 상관 없음.)
+	return 0;
+}
---
+++ 윤이삭/2주차_C/윤이삭_2주차_3.c
@@ -0,0 +1,44 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+void ClearBuffer()
+{
+	while (getchar() != '\n');
+}
+
+
+int main()
+{
+	char *str = (char *)malloc(sizeof(char) * 200);			// 문장을 입력받을 때 동적 할당 사용.
+	char *findstr = (char *)malloc(sizeof(char) * 20);
+	int i = 0, count = 0, length = 0;
+	printf("문자열을 입력하세요 : ");
+	gets(str);
+	char *token = strtok(str, " ");		// 공백을 기준으로 단어를 나누어주는 strtok 함수 사용
+										// 참고 : https://dojang.io/mod/page/view.php?id=381
+	length = strlen(str);
+
+	printf("찾고 싶은 단어는 무엇인가요? : ");
+	scanf("%s", findstr);
+	ClearBuffer();
+
+	while (token != NULL)
+	{
+		count++;
+		if (strcmp(token, findstr) == 0)		// strcmp로 단어 비교. token == findstr를 사용하면 결과가 나오지 않는다.
+		{
+			printf("단어 %s(은)는 %d번째 위치에 있습니다. \n", findstr, count);
+			break;
+		}
+		//printf("%s\n", token);
+		
+		token = strtok(NULL, " ");
+
+	}
+
+	free(str);
+	free(findstr);
+	return 0;
+}
