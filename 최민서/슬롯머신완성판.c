acfdd93fb68f5f556ee34784d787ee89bc9380e7
CMS1108 <37628681+CMS1108@users.noreply.github.com>
2018-05-07 09:40:02 +0900

    슬롯머신완성판
    슬롯머신완성판

---
+++ 최민서/슬롯머신완성판.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+	int start = 0;
+	int money = 1000;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	printf("게임을 플레이하시려면 초기 소지금을 적어주세요.(최소 100원 이상) => ");
+	scanf_s("%d", &money);
+	money = money;
+	if (money < 100)
+	{
+		printf("소지금이 부족하여 게임을 진행할 수 없습니다.\n");
+		system("pause");
+		return 0;
+	}
+	printf("초기 소지금은 %d원이며, 게임 시작 시 100원 감소합니다.\n", money);
+	printf("게임 진행을 원하시면 1을 입력, 중단하시려면 0을 입력하세요.\n");
+	scanf_s("%d", &start);
+	while (start>1)
+	{
+		printf("1을 누르면 다시시작\n");
+		scanf_s("%d", &start);
+	}
+	while (start == 1)
+	{
+		srand(time(NULL)); // 난수 초기화
+		b = rand() % 10; // 0~9 중의 숫자 랜덤 지정
+		c = rand() % 10;
+		d = rand() % 10;
+		printf("<< %d %d %d >>\n", b, c, d);
+		money = money - 100;
+		if (b == c && c == d)
+		{
+			money = money + 1100;
+			printf("당첨입니다. 상금 1000원을 얻으셨습니다.\n");
+		}
+		start++;
+		printf("남은돈은 %d원입니다.\n", money);
+		printf("게임을 계속하시려면 1을 입력, 중단하시려면 0을 입력하세요.\n");
+		scanf_s("%d", &start);
+		while (start > 1)
+		{
+			printf("1을 누르면 다시시작\n");
+			scanf_s("%d", &start);
+		}
+		if (money < 100)
+		{
+			printf("소지금이 게임 참가비보다 적으므로 게임을 종료합니다.\n");
+			system("pause");
+			return 0;
+		}
+	}
+	return 0;
+}
